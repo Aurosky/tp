a9413de18b1c03dd310b30bf9d7b2f2f685bd28b
prerana-r11 <preranaravi2006@gmail.com>
2026-03-17 14:34:25 +0800

    Add Gift class and gift commands

--- src/main/java/seedu/duke/data/child/Child.java
+++ src/main/java/seedu/duke/data/child/Child.java
@@ -1,12 +1,29 @@
 package seedu.duke.data.child;
 
+import java.util.ArrayList;
+import seedu.duke.data.Gift.Gift;
+
 //Solution below inspired by https://github.com/se-edu/addressbook-level2/blob/master/src/seedu/addressbook/data
 // /person/Person.java
 public class Child implements ReadOnlyChild{
     private Name name;
+    private ArrayList<Gift> gifts;
 
     public Child(Name name) {
         this.name = name;
+        this.gifts=new ArrayList<>();
+    }
+    public void addGift(Gift gift){
+        gifts.add(gift);
+    }
+    public Gift getGift(int index){
+        return gifts.get(index);
+    }
+    public void removeGift(int index){
+        gifts.remove(index);
+    }
+    public ArrayList<Gift> getGifts(){
+        return gifts;
     }
 
     @Override
