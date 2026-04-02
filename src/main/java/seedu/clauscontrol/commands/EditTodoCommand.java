790316480099ddc4664afa593f2dc7509e3eddae
shrabasti <shrabastichakraborty16@gmail.com>
2026-04-02 11:01:45 +0530

    Update logging of edittodo command

--- src/main/java/seedu/clauscontrol/commands/EditTodoCommand.java
+++ src/main/java/seedu/clauscontrol/commands/EditTodoCommand.java
@@ -4,6 +4,8 @@ import seedu.clauscontrol.data.exception.IllegalValueException;
 import seedu.clauscontrol.data.todo.Todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 //@@author shrabasti-c
 /**
@@ -14,17 +16,20 @@ public class EditTodoCommand extends Command {
     private final int index;
     private final String newDescription;
     private final LocalDate newDeadline;
+    private static final Logger logger = Logger.getLogger("Foo");
 
     public EditTodoCommand(ArrayList<Todo> todoList, int index, String newDescription, LocalDate newDeadline) {
         this.todoList = todoList;
         this.index = index;
         this.newDescription = newDescription;
         this.newDeadline = newDeadline;
+        logger.log(Level.INFO, "successful edit todo instantiation");
     }
 
     @Override
     public String execute() {
         if (index < 0 || index >= todoList.size()) {
+            logger.log(Level.WARNING, "unsuccessful edit todo execution");
             return "Invalid position :(";
         }
 
@@ -40,8 +45,10 @@ public class EditTodoCommand extends Command {
             }
 
         } catch (IllegalValueException e) {
+            logger.log(Level.INFO, "unsuccessful edit todo execution");
             throw new RuntimeException(e);
         }
+        logger.log(Level.INFO, "successful edit todo execution");
         return "Todo Details changed!";
     }
 }
